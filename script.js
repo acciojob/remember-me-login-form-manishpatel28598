2263c068c387bca1c5783d1873cd8f0a3a7de9bd
Manish kumar <119885517+manishpatel28598@users.noreply.github.com>
2024-03-26 19:33:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your JS code here. If required.
+  window.onload = function() {
+        if (localStorage.getItem("username") && localStorage.getItem("password")) {
+            var existingButton = document.createElement("button");
+            existingButton.id = "existing";
+            existingButton.innerText = "Login as existing user";
+            existingButton.addEventListener("click", function() {
+                alert("Logged in as " + localStorage.getItem("username"));
+            });
+            document.body.appendChild(existingButton);
+        }
+    };
+
+    // Submit function
+    document.getElementById("loginForm").addEventListener("submit", function(event) {
+        event.preventDefault();
+        var username = document.getElementById("username").value;
+        var password = document.getElementById("password").value;
+        var remember = document.getElementById("checkbox").checked;
+
+        if (remember) {
+            localStorage.setItem("username", username);
+            localStorage.setItem("password", password);
+        } else {
+            localStorage.removeItem("username");
+            localStorage.removeItem("password");
+        }
+
+        alert("Logged in as " + username);
+
+        // Reset form
+        document.getElementById("loginForm").reset();
+    });
